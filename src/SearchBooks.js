0afd8c22f4eb438fa785774ee9a7670c874b7f10
Bernadete <beteaquino.jf@gmail.com>
2019-03-04 08:18:38 -0300

    Adicionando busca por livros

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 import './App.css'
 
 class SearchBooks extends Component {
+    state = {
+        books: [],
+        firstRender: true
+    }
+
+    search = (query) => {
+        this.setState(() => ({
+            firstRender: false
+        }))
+
+        if (query && query.length > 1) {
+            BooksAPI.search(query)
+            .then((result) => {
+                var books = !('error' in result) ? result : []
+                this.setState(() => ({
+                    books
+                }))
+            })
+        }      
+    }
+
     render() {
         return(
             <div className="search-books">
@@ -12,19 +35,28 @@ class SearchBooks extends Component {
                         className="close-search"
                         >Close</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
-                        <input type="text" placeholder="Search by title or author"/>
+                        <input 
+                            type="text"
+                            placeholder="Search by title or author"
+                            onChange={(event) => this.search(event.target.value)}/>
                     </div>
                 </div>
+                
                 <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                    {this.state.books.length > 0 && (
+                        <ol className="books-grid">
+                            {this.state.books.map((book) => (
+                                <li key={book.id}>
+                                    <Book book={book}/>
+                                </li>
+                            ))}
+                        </ol>
+                   )}
+                   {this.state.firstRender === false && this.state.books.length === 0 && (
+                       <div className="no-results">
+                           Nenhum resultado encontrado!
+                        </div>
+                   )}
                 </div>
             </div>
         )
