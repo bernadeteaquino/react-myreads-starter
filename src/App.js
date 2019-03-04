cd9f8d088a4ba99b45d7838fd8d2294a9aa3c219
Bernadete <beteaquino.jf@gmail.com>
2019-03-04 11:19:50 -0300

    Adicionando listagem de livros

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React from 'react'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    booksOnTheShelf: {}
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((result) => {
+      const booksOnTheShelf = {};
+      result.forEach(book => {
+        booksOnTheShelf[book.shelf] = booksOnTheShelf[book.shelf] || []
+        booksOnTheShelf[book.shelf].push(book) 
+      });
+
+      this.setState(() => ({
+        booksOnTheShelf
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={ListBooks}/>
+        <Route exact path='/' render={() => (
+          <ListBooks
+            booksOnTheShelf={this.state.booksOnTheShelf}
+          />
+        )} />
         <Route exact path="/search" component={SearchBooks}/>
       </div>
     )
